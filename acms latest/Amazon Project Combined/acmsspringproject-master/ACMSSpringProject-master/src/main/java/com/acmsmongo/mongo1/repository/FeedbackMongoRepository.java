d1e3e2a20070f7e7d952fc203331fea6c7dfeb56
Shuchita Dobhal <31426705+dobhsh@users.noreply.github.com>
2019-06-18 22:42:35 +0530

    Delete FeedbackMongoRepository.java

--- acms latest/Amazon Project Combined/acmsspringproject-master/ACMSSpringProject-master/src/main/java/com/acmsmongo/mongo1/repository/FeedbackMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acmsmongo.mongo1.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.acmsmongo.mongo1.model.Feedback;
-
-public interface FeedbackMongoRepository extends CrudRepository<Feedback, String>{}
